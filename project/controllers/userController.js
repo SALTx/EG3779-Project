9d55eea07920128db19fe80a542ffeada9135174
Saahil <29069313+SALTx@users.noreply.github.com>
2020-07-24 11:37:06 +0800

    24-7

--- project/controllers/userController.js
+++ project/controllers/userController.js
@@ -1,39 +1,44 @@
 // let CustomerModel = require('./../models/users/customer');
 // let ShopOwnerModel = require('./../models/users/shopowner');
-let UserModel = require('./../models/users/user');
+let UserModel = require("./../models/users/user");
 
 class User {
-    constructor(user) {
-        this.username = user.username;
-        this.password = user.password;
-        this.bio = user.bio;
-    }
+  constructor(user) {
+    this.username = user.username;
+    this.password = user.password;
+    this.bio = user.bio;
+  }
 }
 
-function validateRegister(user) {
-
-}
+function validateRegister(user) {}
 
 module.exports = {
-    registerUser: function (user) {
-        if(validateRegister(user)) { return false; }
-        let username = user.username;
-        let email = user.email;
-        let password = user.password;
-        let phoneNumber = user.phoneNumber;
-        let image = user.image;
+  registerUser: function (user) {
+    if (validateRegister(user)) {
+      return false;
+    }
+    let username = user.username;
+    let email = user.email;
+    let password = user.password;
+    let phoneNumber = user.phoneNumber;
+    let image = user.image;
 
-        const newUser = new UserModel({
-            username,
-            email,
-            password,
-            phoneNumber,
-            image,
-        });
+    const newUser = new UserModel({
+      username,
+      email,
+      password,
+      phoneNumber,
+      image,
+    });
 
-        newUser.save({}, function (e) { console.log(e) });
-    },
-    login: function (username, password) {
-        
-    },
-}
+    newUser.save({}, function (e) {
+      console.log(e);
+    });
+  },
+  login: function (username, password) {
+    return UserModel.find({
+      username: username,
+      password: password,
+    }).limit(1);
+  },
+};
--- project/routes.js
+++ project/routes.js
@@ -1,55 +1,82 @@
-let bodyParser = require('body-parser');
+let bodyParser = require("body-parser");
 //import controllers here
-let storeController = require('./controllers/storeController');
-let userController = require('./controllers/userController');
+let storeController = require("./controllers/storeController");
+let userController = require("./controllers/userController");
 
-let database = require('./models/database');
+let database = require("./models/database");
 database.connect();
 
-let router = require('express').Router();
+let router = require("express").Router();
 
-router.use(bodyParser.urlencoded({
+router.use(
+  bodyParser.urlencoded({
     extended: true,
-}));
-router.get('/css/*', function (req, res) {
-    res.sendFile(`${__dirname}/views/${req.originalUrl}`);
+  })
+);
+router.get("/css/*", function (req, res) {
+  res.sendFile(`${__dirname}/views/${req.originalUrl}`);
 });
-router.get('/js/*', function (req, res) {
-    res.sendFile(`${__dirname}/views/${req.originalUrl}`);
+router.get("/js/*", function (req, res) {
+  res.sendFile(`${__dirname}/views/${req.originalUrl}`);
 });
-router.get('/static/*', function (req, res) {
-    console.log()
-    res.sendFile(`${__dirname}/views/${req.originalUrl}`);
+router.get("/static/*", function (req, res) {
+  console.log();
+  res.sendFile(`${__dirname}/views/${req.originalUrl}`);
 });
-router.get('/resources/*', function (req, res) {
-    console.log('getting resource');
-    res.sendFile(`${__dirname}/${req.originalUrl}`);
+router.get("/resources/*", function (req, res) {
+  console.log("getting resource");
+  res.sendFile(`${__dirname}/${req.originalUrl}`);
 });
 
 //index
-router.get('/', function (req, res) {
-    res.sendFile(`${__dirname}/views/index.html`);
-})
+router.get("/", function (req, res) {
+  res.sendFile(`${__dirname}/views/index.html`);
+});
 
 //login and register
-router.get('/register', function (Req, res) {
-    res.sendFile(`${__dirname}/views/register.html`);
-})
-router.post('/register', function (req, res) {
-    let data = req.body;
+router.get("/register", function (req, res) {
+  res.sendFile(`${__dirname}/views/register.html`);
+});
+router.post("/register", function (req, res) {
+  let data = req.body;
 
-    let newUser = {
-        username: data.username,
-        email: data.email,
-        password: data.password,
-        phoneNumber: data.phoneNumber,
-        image: data.image,
-    };
+  let newUser = {
+    username: data.username,
+    email: data.email,
+    password: data.password,
+    phoneNumber: data.phoneNumber,
+    image: data.image,
+  };
 
-    userController.registerUser(newUser);
-    console.log('new user added to DB');
-    res.redirect('back');
-})
+  userController.registerUser(newUser);
+  console.log("new user added to DB");
+  res.redirect("back");
+});
 
+router.get("/login", function (req, res) {
+  res.sendfile(`${__dirname}/views/login.html`);
+});
+router.post("/login", function (req, res) {
+  let data = req.body;
+  let username = data.username;
+  let password = data.password;
+
+  if (userController.login(username, password)) {
+    res.redirect("success.html");
+  } else {
+    res.redirect("fail.html");
+  }
+});
+
+router.get("/fail.html", function (req, res) {
+  res.sendfile(`${__dirname}/views/fail.html`);
+});
+router.get("/success.html", function (req, res) {
+  res.sendfile(`${__dirname}/views/success.html`);
+});
+
+router.get("/contact", function (req, res) {
+  res.sendfile(`${__dirname}/views/contact.html`);
+});
 
 module.exports = router;
